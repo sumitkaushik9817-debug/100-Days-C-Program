d2304bd5f06d7a3b5bf03650831852a00dfb0fc9
Sumit <sumitkaushik9817@gmail.com>
2026-08-27 18:47:45 +0530

    Add files via upload

---
+++ ques13.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+ int main() {
+     int year;
+	 
+	 printf("year");
+	 scanf("%d", &year);
+	 
+	 if (year % 4 == 0)
+		 printf("not a leap year");
+	 else if (year % 100 == 0)
+		 printf("not a leap year");
+	 else if (year % 400 == 0)
+		 printf("leap year");
+	 else
+		 printf("not a leap year");
+	 
+	 return 0;
+ }
